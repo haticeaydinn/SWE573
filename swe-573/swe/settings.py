6a71c6e63cd6164146299d92de98feec4153a178
haticeaydin <hatice3178@yahoo.com.tr>
2021-01-30 00:20:38 +0300

    postgresql settings completed

--- swe-573/swe/settings.py
+++ swe-573/swe/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'swe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fsthbxux',
+        'USER': 'fsthbxux',
+        'PASSWORD': '5jE--b5Ai8oHX2xFKYqZOkYcYLbrnfwZ',
+        'HOST': 'kandula.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
